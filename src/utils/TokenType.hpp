d235b85f6d1f660efc92da35bdae00091098a46d
Victor Miguel <vmmc5910@gmail.com>
2024-05-25 15:27:40 -0300

    Add first version of the 'TokenType'.hpp file.

--- src/utils/TokenType.hpp
+++ src/utils/TokenType.hpp
@@ -4,17 +4,47 @@
 
 enum TokenType{
   // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN,
+  LEFT_BRACE, RIGHT_BRACE,
+  LEFT_BRACKET, RIGHT_BRACKET,
+  COMMA, DOT, INTERROGATION, COLON, SEMICOLON,
+  PLUS, MINUS, STAR, SLASH,
 
   // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
 
   // Literals.
+  IDENTIFIER, NUMBER, STRING,
 
   // Keywords.
+  AND, BREAK, CLASS, CONTINUE, DO, ELSE, ELSIF,
+  FALSE, FUNCTION, FOR, IF, LAMBDA, LET, METHOD,
+  NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, WHILE,
 
   // File End.
+  FILE_END
 };
 
-std::string toString(TokenType){
+std::string toString(TokenType type){
+  static const std::string strings[] = {
+    "LEFT_PAREN", "RIGHT_PAREN",
+    "LEFT_BRACE", "RIGHT_BRACE",
+    "LEFT_BRACKET", "RIGHT_BRACKET",
+    "COMMA", "DOT", "INTERROGATION", "COLON", "SEMICOLON",
+    "PLUS", "MINUS", "STAR", "SLASH",
+    "BANG", "BANG_EQUAL",
+    "EQUAL", "EQUAL_EQUAL",
+    "GREATER", "GREATER_EQUAL",
+    "LESS", "LESS_EQUAL",
+    "IDENTIFIER", "NUMBER", "STRING",
+    "AND", "BREAK", "CLASS", "CONTINUE", "DO", "ELSE", "ELIF",
+    "FALSE", "FUNCTION", "FOR", "IF", "LAMBDA", "LET", "METHOD",
+    "NIL", "OR", "PRINT", "RETURN", "SUPER", "THIS", "TRUE", "WHILE",
+    "FILE_END"
+  };
 
-  return;
+  return strings[static_cast<int>(type)];
 }
