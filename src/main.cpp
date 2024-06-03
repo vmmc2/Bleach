2d1f81fefdb6f539aafdace7d4c3b78a7b4db7a2
Victor Miguel <vmmc5910@gmail.com>
2024-06-03 14:09:41 -0300

    Add complete documentation to the 'main' and 'readFile' functions from the 'main.cpp' file.

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,19 @@
 #include "parser/Parser.hpp"
 
 /**
- * @brief 
+ * @brief Receives a path to a file, checks whether the file exists and if it is a Bleach file, extracts all of
+ * its content and stores it in a string. Then, return such string.
  * 
- * @param 
+ * This function is responsible for receiving the relative file path of a supposed Bleach file. Then, it checks
+ * whether the file exists and whether it is, indeeed, a Bleach file. Then, it tries to open the said file. If
+ * everything went okay, then this function extracts the whole content of the file, stores it inside a string
+ * (std::string) variable called "fileContent" and returns it.
  * 
- * @return 
+ * @param filePath A string (std::string_view) that stores the relative file path to the Bleach file that will
+ * be interpreted.
+ * 
+ * @return A string (std::string) that contains the content (source code) written inside the file pointed by the
+ * provided file path.
 **/
 std::string readFile(std::string_view filePath){
   std::ifstream file{filePath.data(), std::ios::in | std::ios::binary | std::ios::ate};
@@ -105,11 +113,21 @@ void runPrompt() {
 }
 
 /**
- * @brief 
+ * @brief The entry point of the BLEACH Interpreter. Runs the interpreter in the file mode or in the REPL mode,
+ * depending on whether or not you have passed parameters to the executable when asking the OS to run it.
  * 
- * @param 
+ * This function is responsible for being the entry point of the BLEACH interpreter. It starts up the interpreter
+ * in two possible modes ("File Mode" or "REPL Mode").
+ * To start up the interpreter in the "File Mode", you must compile the project using the provided Makefile and
+ * then provide the relative path of the Bleach file you want to compile as the only argument when executing
+ * the generated binary.
+ * To start up the interpreter in the "REPL Mode", you must compile the project using the provided Makefile and
+ * then just execute the generated binary.
  * 
- * @return 
+ * @param argc: The int that represents the number of arguments passed when running the executable.
+ * @param argv: The array of strings (char*[]) that stores the values of each of the passed arguments.
+ * 
+ * @return An int that denotes whether the 'main' function executed without problems (0) or with problems (a number different from 0).
 **/
 int main(int argc, char* argv[]){
   if(argc == 2){
