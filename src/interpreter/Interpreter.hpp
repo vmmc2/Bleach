2617f9e7b967e5ee3357c8428c1e3c77f7109f53
Victor Miguel <vmmc5910@gmail.com>
2024-06-07 17:03:01 -0300

    Add implementation of several methods that visit and interpret the nodes of the Bleach AST.

--- src/interpreter/Interpreter.hpp
+++ src/interpreter/Interpreter.hpp
@@ -33,6 +33,36 @@ class Interpreter : public ExprVisitor{
      * 
      * @return
      */
+    bool isEqual(const std::any& left, const std::any& right){
+      if(left.type() == typeid(nullptr) && right.type() == typeid(nullptr)){
+        return true;
+      }
+      if((left.type() != typeid(nullptr) && right.type() == typeid(nullptr)) || (left.type() == typeid(nullptr) && right.type() != typeid(nullptr))){
+        return false;
+      }
+      if(left.type() == typeid(bool) && right.type() == typeid(bool)){
+        return std::any_cast<bool>(left) == std::any_cast<bool>(right);
+      }
+      if(left.type() == typeid(double) && right.type() == typeid(double)){
+        return std::any_cast<double>(left) == std::any_cast<double>(right);
+      }
+      if(left.type() == typeid(std::string) && right.type() == typeid(std::string)){
+        return std::any_cast<std::string>(left) == std::any_cast<std::string>(right);
+      }
+      // #TODO: Extend this to deal with 'lists' and 'dicts'.
+
+      return false;
+    }
+
+    /**
+     * @brief Defines 
+     *
+     * This method
+     * 
+     * @param expr:
+     * 
+     * @return
+     */
     bool isTruthy(const std::any& object){
       if(object.type() == typeid(nullptr)){
         return false;
@@ -61,6 +91,18 @@ class Interpreter : public ExprVisitor{
       std::any right = evaluate(expr->right);
 
       switch(expr->op.type){
+        case(TokenType::GREATER):
+          return std::any_cast<double>(left) > std::any_cast<double>(right);
+        case(TokenType::GREATER_EQUAL):
+          return std::any_cast<double>(left) >= std::any_cast<double>(right);
+        case(TokenType::LESS):
+          return std::any_cast<double>(left) < std::any_cast<double>(right);
+        case(TokenType::LESS_EQUAL):
+          return std::any_cast<double>(left) <= std::any_cast<double>(right);
+        case(TokenType::BANG_EQUAL):
+          return !isEqual(left, right);
+        case(TokenType::EQUAL_EQUAL):
+          return isEqual(left, right);
         case(TokenType::PLUS):
           if(left.type() == typeid(double) && right.type() == typeid(double)){
             return std::any_cast<double>(left) + std::any_cast<double>(right);
