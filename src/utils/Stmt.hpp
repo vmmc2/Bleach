03737a24ed77f4bdb3b5085dc0eeae69731e72ad
Victor Miguel <vmmc5910@gmail.com>
2024-06-13 22:48:24 -0300

    Add complete implementation of the 'If' struct from the 'Stmt.hpp' file.

--- src/utils/Stmt.hpp
+++ src/utils/Stmt.hpp
@@ -10,13 +10,15 @@
 
 // Necessary forward declarations of certain structs so they can be used inside the 'StmtVisitor' struct below.
 struct Block;
-struct Expression;
+struct Expression; // Expression statement (Used in function and method calls).
+struct If;
 struct Print;
 struct Var; // Variable declaration statement.
 
 struct StmtVisitor{
   virtual std::any visitBlockStmt(std::shared_ptr<Block> stmt) = 0;
   virtual std::any visitExpressionStmt(std::shared_ptr<Expression> stmt) = 0;
+  virtual std::any visitIfStmt(std::shared_ptr<If> stmt) = 0;
   virtual std::any visitPrintStmt(std::shared_ptr<Print> stmt) = 0;
   virtual std::any visitVarStmt(std::shared_ptr<Var> stmt) = 0;
   virtual ~StmtVisitor() = default;
@@ -47,6 +49,23 @@ struct Expression : Stmt, public std::enable_shared_from_this<Expression>{
 
   std::any accept(StmtVisitor& visitor) override{
     return visitor.visitExpressionStmt(shared_from_this());
+  }
+};
+
+struct If : Stmt, public std::enable_shared_from_this<If>{
+  const std::shared_ptr<Expr> ifCondition;
+  const std::shared_ptr<Stmt> ifBranch;
+  const std::vector<std::shared_ptr<Expr>> elifConditions;
+  const std::vector<std::shared_ptr<Stmt>> elifBranches;
+  const std::shared_ptr<Stmt> elseBranch;
+
+  If(std::shared_ptr<Expr> ifCondition, std::shared_ptr<Stmt> ifBranch, std::vector<std::shared_ptr<Expr>> elifConditions, std::vector<std::shared_ptr<Stmt>> elifBranches, std::shared_ptr<Stmt> elseBranch)
+    : ifCondition{std::move(ifCondition)}, ifBranch{std::move(ifBranch)}, elifConditions{std::move(elifConditions)}, elifBranches{std::move(elifBranches)}, elseBranch{std::move(elseBranch)}
+  {}
+
+
+  std::any accept(StmtVisitor& visitor) override{
+    return visitor.visitIfStmt(shared_from_this());
   }
 };
 
