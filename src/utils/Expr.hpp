950c0e5107d544be2556e64779fc9a59f9eaf941
Victor Miguel <vmmc5910@gmail.com>
2024-06-14 14:30:34 -0300

    Add complete implementation of the 'Logical' struct from the 'Expr.hpp' file.

--- src/utils/Expr.hpp
+++ src/utils/Expr.hpp
@@ -13,6 +13,7 @@ struct Assign;
 struct Binary;
 struct Grouping;
 struct Literal;
+struct Logical;
 struct Ternary;
 struct Unary;
 struct Variable;
@@ -35,6 +36,7 @@ struct ExprVisitor{
   virtual std::any visitBinaryExpr(std::shared_ptr<Binary> expr) = 0;
   virtual std::any visitGroupingExpr(std::shared_ptr<Grouping> expr) = 0;
   virtual std::any visitLiteralExpr(std::shared_ptr<Literal> expr) = 0;
+  virtual std::any visitLogicalExpr(std::shared_ptr<Logical> expr) = 0;
   virtual std::any visitTernaryExpr(std::shared_ptr<Ternary> expr) = 0;
   virtual std::any visitUnaryExpr(std::shared_ptr<Unary> expr) = 0;
   virtual std::any visitVariableExpr(std::shared_ptr<Variable> expr) = 0;
@@ -162,6 +164,20 @@ struct Literal : Expr, public std::enable_shared_from_this<Literal>{
 
   std::any accept(ExprVisitor& visitor) override{
     return visitor.visitLiteralExpr(shared_from_this());
+  }
+};
+
+struct Logical : Expr, public std::enable_shared_from_this<Logical>{
+  const std::shared_ptr<Expr> left;
+  const Token op;
+  const std::shared_ptr<Expr> right;
+
+  Logical(std::shared_ptr<Expr> left, Token op, std::shared_ptr<Expr> right)
+    : left{std::move(left)}, op{std::move(op)}, right{std::move(right)} 
+  {}
+
+  std::any accept(ExprVisitor& visitor) override{
+    return visitor.visitLogicalExpr(shared_from_this());
   }
 };
 
