72f38d54ac6fe6913f69690dd3bf9f1d04ba8cca
Victor Miguel <vmmc5910@gmail.com>
2024-06-17 22:36:05 -0300

    Add complete documentation of the 'Environment' class along with the complete documentation of the 'define' method of this same class, both of which are present inside the 'Environment.hpp' file.

--- src/utils/Environment.hpp
+++ src/utils/Environment.hpp
@@ -9,20 +9,37 @@
 #include "../error/BleachRuntimeError.hpp"
 #include "./Token.hpp"
 
-
+/**
+ * @class Environment
+ * 
+ * @brief Utility class that stores bindings between variables and their respective values.
+ * 
+ * The Environment class is responsible for storing (during the whole execution of a program) the bindings 
+ * between variables' names, that were declared inside the program, and their respective values. This utility
+ * class is very important because it allows the interpreter to "remember" the declared variables.
+ * 
+ * @note: Pay attention to the fact that the "values" attribute, the "std::map" that stores the bindings between
+ * variables and values, uses the "std::string" type as the key type. This is a deliberate decision, because
+ * , inside the same scope, the tokens that represent the same identifier must point to the same value. To do
+ * that, we use the "lexeme" attribute of the "Token" class. By doing so, we make sure that different tokens
+ * with the same lexeme point to the same value, if, and only if, such tokens are in the same scope.
+**/
 class Environment{
   private:
-    std::map<std::string,std::any> values;
+    std::map<std::string,std::any> values; /**< Variable that stores the bindings between variables' names and their associated values. */
     std::shared_ptr<Environment> enclosing;
 
   public:
+
     Environment()
       : enclosing{nullptr}
     {}
 
+
     Environment(std::shared_ptr<Environment> enclosing)
       : enclosing{std::move(enclosing)}
     {}
+
 
     void assign(const Token& name, std::any value){
       auto elem = values.find(name.lexeme);
@@ -40,12 +57,27 @@ class Environment{
       throw BleachRuntimeError{name, "Undefined variable '" + name.lexeme + "'."};
     }
 
-
+    /**
+     * @brief Defines a variable and associates a value to it inside the current environment. 
+     *
+     * This method works as a variable definition method. It essentialy binds the name of the variable to its
+     * respective value.
+     * 
+     * @param name: The name of the variable being defined. Such variable is of type std::string&.
+     * @param value: The value associated with the name of the variable that is being defined. Such variable is
+     * of type std::any.
+     * 
+     * @return Nothing (void).
+     * 
+     * @note: Pay attention to the fact that Bleach has made an interesting semantic choice. We allow the user
+     * to perform variable redefinition, but only inside the global scope.
+     */
     void define(const std::string& name, std::any value){
       values[name] = std::move(value);
 
       return;
     }
+
 
     std::any get(const Token& name){
       auto elem = values.find(name.lexeme);
