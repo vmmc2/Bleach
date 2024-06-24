468214345487426f42fc6f93fdc49ae70e1428e2
Victor Miguel <vmmc5910@gmail.com>
2024-06-24 13:59:40 -0300

    Add complete implementation of the 'BleachCallable' class from the 'BleachCallable.hpp' file.

---
+++ src/utils/BleachCallable.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <any>
+#include <string>
+#include <vector>
+
+class Interpreter;
+
+class BleachCallable{
+  public:
+    virtual int arity() = 0;
+    virtual std::any call(Interpreter& interpreter, std::vector<std::any> arguments) = 0;
+    virtual std::string toString() = 0;
+    virtual ~BleachCallable() = default;
+};
