60761bbb99bdadea5a8841a867f052a6efb6e94d
Victor Miguel <vmmc5910@gmail.com>
2024-06-29 18:29:00 -0300

    Add support for closures inside the declaration of the 'BleachFunction' class from the 'BleachFunction.hpp' file.

--- src/utils/BleachFunction.hpp
+++ src/utils/BleachFunction.hpp
@@ -13,9 +13,10 @@ class Function;
 
 class BleachFunction : public BleachCallable{
   private:
+    std::shared_ptr<Environment> closure;
     std::shared_ptr<Function> functionDeclaration;
   public:
-    BleachFunction(std::shared_ptr<Function> functionDeclaration);
+    BleachFunction(std::shared_ptr<Function> functionDeclaration, std::shared_ptr<Environment> closure);
     int arity() override;
     std::any call(Interpreter& interpreter, std::vector<std::any> arguments) override;
     std::string toString() override;
