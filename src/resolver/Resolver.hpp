b79801bdab14846c6b197b32b4749330c0f95294
Victor Miguel <vmmc5910@gmail.com>
2024-06-30 21:20:13 -0300

    Add the initial class necessary for the implementation of the 'Resolver' class from the 'Resolver.hpp' file.

---
+++ src/resolver/Resolver.hpp
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "../interpreter/Interpreter.hpp"
+
+class Resolver : public ExprVisitor, public StmtVisitor{
+  private:
+    Interpreter& interpreter;
+    std::vector<std::map<std::string, bool>> scopes;
+
+    void beginScope(){
+      scopes.push_back(std::map<std::string, bool>{});
+
+      return;
+    }
+
+    void endScope(){
+      scopes.pop_back();
+
+      return;
+    }
+
+    void resolve(const std::shared_ptr<Expr>& expression){
+      expression->accept(*this);
+
+      return;
+    }
+
+    void resolve(const std::shared_ptr<Stmt>& statement){
+      statement->accept(*this);
+
+      return;
+    }
+
+  public:
+    Resolver(Interpreter& interpreter)
+      : interpreter{interpreter}
+    {}
+
+    void resolve(const std::vector<std::shared_ptr<Stmt>>& statements){
+      for(const std::shared_ptr<Stmt>& statement : statements){
+        resolve(statement);
+      }
+
+      return;
+    }
+
+    std::any visitAssignExpr(std::shared_ptr<Assign> stmt) override{
+
+      return {};
+    }
+
+    std::any visitBinaryExpr(std::shared_ptr<Binary> stmt) override{
+
+      return {};
+    }
+
+    std::any visitCallExpr(std::shared_ptr<Call> stmt) override{
+
+      return {};
+    }
+
+    std::any visitGroupingExpr(std::shared_ptr<Grouping> stmt) override{
+
+      return {};
+    }
+
+    std::any visitLambdaFunctionExpr(std::shared_ptr<LambdaFunction> stmt) override{
+
+      return {};
+    }
+
+    std::any visitLiteralExpr(std::shared_ptr<Literal> stmt) override{
+
+      return {};
+    }
+
+    std::any visitLogicalExpr(std::shared_ptr<Logical> stmt) override{
+
+      return {};
+    }
+
+    std::any visitTernaryExpr(std::shared_ptr<Ternary> stmt) override{
+
+      return {};
+    }
+
+    std::any visitUnaryExpr(std::shared_ptr<Unary> stmt) override{
+
+      return {};
+    }
+
+    std::any visitVariableExpr(std::shared_ptr<Variable> stmt) override{
+
+      return {};
+    }
+
+    std::any visitBlockStmt(std::shared_ptr<Block> stmt) override{
+      beginScope();
+      resolve(stmt->statements);
+      endScope();
+
+      return {};
+    }
+
+    std::any visitDoWhileStmt(std::shared_ptr<DoWhile> stmt) override{
+
+      return {};
+    }
+
+    std::any visitExpressionStmt(std::shared_ptr<Expression> stmt) override{
+
+      return {};
+    }
+
+    std::any visitFunctionStmt(std::shared_ptr<Function> stmt) override{
+
+      return {};
+    }
+
+    std::any visitIfStmt(std::shared_ptr<If> stmt) override{
+
+      return {};
+    }
+
+    std::any visitPrintStmt(std::shared_ptr<Print> stmt) override{
+
+      return {};
+    }
+
+    std::any visitReturnStmt(std::shared_ptr<Return> stmt) override{
+
+      return {};
+    }
+
+    std::any visitVarStmt(std::shared_ptr<Var> stmt) override{
+
+      return {};
+    }
+
+    std::any visitWhileStmt(std::shared_ptr<While> stmt) override{
+
+      return {};
+    }
+};
