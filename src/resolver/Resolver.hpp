b729c62021982c238675c1b75e94db82eb282982
Victor Miguel <vmmc5910@gmail.com>
2024-07-02 00:52:13 -0300

    Add complete implementation of the 'resolveLocal' and 'visitVariableExpr' methods of the 'Resolver' class from the 'Resolver.hpp' file.

--- src/resolver/Resolver.hpp
+++ src/resolver/Resolver.hpp
@@ -57,6 +57,18 @@ class Resolver : public ExprVisitor, public StmtVisitor{
       return;
     }
 
+    void resolveLocal(std::shared_ptr<Expr> expr, const Token& name){
+      for(int i = scopes.size() - 1; i >= 0; i--){
+        if(scopes[i].find(name.lexeme) != scopes[i].end()){
+          interpreter.resolve(expr, scopes.size() - 1 - i); // This tells the interpreter how many hops it will need to do in order to find the variable declaration to which this Variable expression is referring to. 
+          // We pass the Variable expression pointer because if the token was passed, it would not work properly. A pointer is unique. A Token is not.
+          return;
+        }
+      }
+
+      return;
+    }
+
   public:
     Resolver(Interpreter& interpreter)
       : interpreter{interpreter}
@@ -70,52 +82,61 @@ class Resolver : public ExprVisitor, public StmtVisitor{
       return;
     }
 
-    std::any visitAssignExpr(std::shared_ptr<Assign> stmt) override{
+    std::any visitAssignExpr(std::shared_ptr<Assign> expr) override{
 
       return {};
     }
 
-    std::any visitBinaryExpr(std::shared_ptr<Binary> stmt) override{
+    std::any visitBinaryExpr(std::shared_ptr<Binary> expr) override{
 
       return {};
     }
 
-    std::any visitCallExpr(std::shared_ptr<Call> stmt) override{
+    std::any visitCallExpr(std::shared_ptr<Call> expr) override{
 
       return {};
     }
 
-    std::any visitGroupingExpr(std::shared_ptr<Grouping> stmt) override{
+    std::any visitGroupingExpr(std::shared_ptr<Grouping> expr) override{
 
       return {};
     }
 
-    std::any visitLambdaFunctionExpr(std::shared_ptr<LambdaFunction> stmt) override{
+    std::any visitLambdaFunctionExpr(std::shared_ptr<LambdaFunction> expr) override{
 
       return {};
     }
 
-    std::any visitLiteralExpr(std::shared_ptr<Literal> stmt) override{
+    std::any visitLiteralExpr(std::shared_ptr<Literal> expr) override{
 
       return {};
     }
 
-    std::any visitLogicalExpr(std::shared_ptr<Logical> stmt) override{
+    std::any visitLogicalExpr(std::shared_ptr<Logical> expr) override{
 
       return {};
     }
 
-    std::any visitTernaryExpr(std::shared_ptr<Ternary> stmt) override{
+    std::any visitTernaryExpr(std::shared_ptr<Ternary> expr) override{
 
       return {};
     }
 
-    std::any visitUnaryExpr(std::shared_ptr<Unary> stmt) override{
+    std::any visitUnaryExpr(std::shared_ptr<Unary> expr) override{
 
       return {};
     }
 
-    std::any visitVariableExpr(std::shared_ptr<Variable> stmt) override{
+    std::any visitVariableExpr(std::shared_ptr<Variable> expr) override{
+      if(!scopes.empty()){
+        auto& scope = scopes.back();
+        auto elem = scope.find(expr->name.lexeme);
+        if(elem != scope.end() && elem->second == false){ // Remember: If the interpreter is visiting this node, then its visiting a name that references a variable inside an expression.
+          error(expr->name, "Cannot read local variable in its own initializer."); // If the variable that it refers to has a false value associated to it in the scope, then it means we are inside an initializer using a variable that is refering to the variable that is being declared. Not allowed.
+        }
+      }
+
+      resolveLocal(expr, expr->name);
 
       return {};
     }
@@ -159,11 +180,11 @@ class Resolver : public ExprVisitor, public StmtVisitor{
     }
 
     std::any visitVarStmt(std::shared_ptr<Var> stmt) override{
-      declare(stmt->name);
+      declare(stmt->name); // First, a variable is declared. (Its associated value in the scope is false).
       if(stmt->initializer != nullptr){ // If a value is assigned to the variable in its declaration.
-        resolve(stmt->initializer);
+        resolve(stmt->initializer); // We then need to resolve the initializer expression. However, it might be possible that the initializer refers to a variable that has the same name as the variable being declared.
       }
-      define(stmt->name);
+      define(stmt->name); // After declaring the variable, resolving its possible initializer, we can define it (Its associated value in the scope is now true).
 
       return {};
     }
