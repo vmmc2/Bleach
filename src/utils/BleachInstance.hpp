5f6cc1e264f9f17a22cf3a207d2f9594b3c898df
Victor Miguel <vmmc5910@gmail.com>
2024-07-15 00:35:10 -0300

    Update the content of the 'BleachInstance.hpp' file.

--- src/utils/BleachInstance.hpp
+++ src/utils/BleachInstance.hpp
@@ -12,7 +12,9 @@ class Token;
 class BleachInstance : public std::enable_shared_from_this<BleachInstance>{
   private:
     std::shared_ptr<BleachClass> klass;
+    std::map<std::string, std::any> fields;
   public:
     BleachInstance(std::shared_ptr<BleachClass> klass);
+    std::any get(const Token& name);
     std::string toString();
 };
