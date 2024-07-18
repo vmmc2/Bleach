77b3c89ed4033cdea115cca5e0cd764ac4f0b6e5
Victor Miguel <vmmc5910@gmail.com>
2024-07-17 23:36:28 -0300

    Add partial implementation of the 'get' method of the 'BleachInstance' class inside the 'BleachInstance.cpp' file.

--- src/utils/BleachInstance.cpp
+++ src/utils/BleachInstance.cpp
@@ -1,11 +1,21 @@
 #include <utility>
 
 #include "./BleachInstance.hpp"
+#include "../error/BleachRuntimeError.hpp"
 
 
 BleachInstance::BleachInstance(std::shared_ptr<BleachClass> klass)
   : klass{std::move(klass)}
 {}
+
+std::any BleachInstance::get(const Token& name){
+  auto elem = fields.find(name.lexeme);
+  if(elem != fields.end()){
+    return elem->second;
+  }
+
+  throw BleachRuntimeError{name, "Undefined property '" + name.lexeme + "'."};
+}
 
 std::string BleachInstance::toString(){
   return "class instance"; // Must change this later...
--- src/utils/BleachInstance.hpp
+++ src/utils/BleachInstance.hpp
@@ -12,7 +12,7 @@ class Token;
 class BleachInstance : public std::enable_shared_from_this<BleachInstance>{
   private:
     std::shared_ptr<BleachClass> klass;
-    std::map<std::string, std::any> fields;
+    std::map<std::string, std::any> fields; // Do not forget that this is a runtime representation of an instance/object. That's why we use the "std::any" type here.
   public:
     BleachInstance(std::shared_ptr<BleachClass> klass);
     std::any get(const Token& name);
