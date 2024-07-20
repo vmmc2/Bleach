41d7ad91c047c8ee6d1f3b83e08de7eacf50836f
Victor Miguel <vmmc5910@gmail.com>
2024-07-20 16:42:34 -0300

    Add an update to the 'get' method of the 'BleachInstance' class.

--- src/utils/BleachInstance.cpp
+++ src/utils/BleachInstance.cpp
@@ -9,9 +9,17 @@ BleachInstance::BleachInstance(std::shared_ptr<BleachClass> klass)
 {}
 
 std::any BleachInstance::get(const Token& name){
+  // When some property of an instance is accessed, first we check if its a field/attribute.
   auto elem = fields.find(name.lexeme);
   if(elem != fields.end()){
     return elem->second;
+  }
+
+  // If that's not the case, then we check whether it's a method from the class of the instance.
+  // In short, fields/attributes shadow methods.
+  auto method = klass->findMethod(name.lexeme);
+  if(method != nullptr){
+    return method;
   }
 
   throw BleachRuntimeError{name, "Undefined property '" + name.lexeme + "'."};
