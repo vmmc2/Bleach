efb4ca989eabfd8b8502d668caad8a175d4f8097
Victor Miguel <vmmc5910@gmail.com>
2024-08-02 15:30:53 -0300

    Add an overloaded version of the 'call' method to the 'BleachCallable' class.

--- src/utils/BleachCallable.hpp
+++ src/utils/BleachCallable.hpp
@@ -6,11 +6,13 @@
 
 
 class Interpreter;
+class Token;
 
 class BleachCallable{
   public:
     virtual int arity() = 0;
-    virtual std::any call(Interpreter& interpreter, std::vector<std::any> arguments) = 0;
+    virtual std::any call(Interpreter& interpreter, std::vector<std::any> arguments) = 0; // To deal with Bleach Classes, Bleach Functions and Bleach Lambda Functions.
+    virtual std::any call(Interpreter& interpreter, Token paren, std::vector<std::any> arguments) = 0; // To deal with Bleach Native Functions.
     virtual std::string toString() = 0;
     virtual ~BleachCallable() = default;
 };
