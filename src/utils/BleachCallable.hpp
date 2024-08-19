3ad972c875791af303d8627b25fc4e04fef5aba4
Victor Miguel <vmmc5910@gmail.com>
2024-08-19 14:44:22 -0300

    Fix typo inside the documentation of the 'BleachCallable' class from the 'BleachCallable.hpp' file.

--- src/utils/BleachCallable.hpp
+++ src/utils/BleachCallable.hpp
@@ -17,7 +17,7 @@ class Token; // Forward declaration necessary to implement the BleachCallable cl
  * The BleachCallable class is responsible for providing an "interface" so the callables of the Bleach language
  * can be properly implemented. Remember that a callable is any entity in a programming language that can be 
  * "called" like a function. This means that such entities can be invoked with a pair of parentheses and 
- * possibly with arguments inside such pair of parentheses . In Bleach's case, this includes: functions, methods,
+ * possibly with arguments inside such pair of parentheses. In Bleach's case, this includes: functions, methods,
  * lambda (anonymous) functions, native functions and classes (when instantiating an object). It doesn't have 
  * any attributes. Instead, it just has virtual methods that must be implemented by any class that inherits from
  * this class.
