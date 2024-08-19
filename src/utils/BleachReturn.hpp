67e5d01286b9b5ce3ea34311215847b4d159522c
Victor Miguel <vmmc5910@gmail.com>
2024-08-19 11:13:55 -0300

    Add the complete documentation of the 'BleachReturn' struct from the 'BleachReturn.hpp' file.

--- src/utils/BleachReturn.hpp
+++ src/utils/BleachReturn.hpp
@@ -2,6 +2,17 @@
 
 #include <any>
 
+/**
+ * @struct BleachReturn
+ * @brief Represents a return statement during runtime (the execution of a Bleach program).
+ *
+ * The BleachReturn struct is used to represent the behavior of a return statement during runtime. During the
+ * execution of a Bleach program, if a return statement is found, then such struct will be thrown as if it was a
+ * runtime error. Moreover, it's important to remember that a return statement might have (or might not have) a
+ * value. Therefore, the need for an attribute called "value". Such attribute is responsible for storing the
+ * value present in a return statement, if any. If that's not the case, then such attribute will store the
+ * value nullptr by default.
+ */
 struct BleachReturn{
-  std::any value;
+  std::any value; // Attribute responsible for storing the value that might be present in a return statement.
 };
