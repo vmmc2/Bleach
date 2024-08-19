2bbb2ef0e8188eafbb4b1ba747ea364aa9baf026
Victor Miguel <vmmc5910@gmail.com>
2024-08-19 10:57:34 -0300

    Fix a typo inside the documentation of the 'BleachContinue' class from the 'BleachContinue.hpp' file.

--- src/utils/BleachContinue.hpp
+++ src/utils/BleachContinue.hpp
@@ -7,7 +7,7 @@
  * The BleachContinue struct is used to represent the behavior of a continue statement during runtime. During 
  * the execution of a Bleach program, if a continue statement is found, then such struct will be thrown as if it 
  * was a runtime error. Inside a loop structure, the Bleach Interpreter tries to catch this type of "runtime 
- * error" in order to signal that a loop has stopped the execution of its body in the middle of it and needs
- * to immediately go to its next iteration.
+ * error" in order to signal that a loop has stopped the execution of its body abruptly and needs to immediately
+ * go to its next iteration.
  */
 struct BleachContinue{};
