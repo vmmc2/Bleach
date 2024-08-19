fa97a1ef43eaeab9a459099d92289438cc21a0ee
Victor Miguel <vmmc5910@gmail.com>
2024-08-19 09:48:12 -0300

    Add complete documentation of the 'BleachBreak' struct from the 'BleachBreak.hpp' file.

--- src/utils/BleachBreak.hpp
+++ src/utils/BleachBreak.hpp
@@ -1,3 +1,13 @@
 #pragma once
 
+
+/**
+ * @struct BleachBreak
+ * @brief Represents a break statement during runtime (the execution of a Bleach program).
+ *
+ * The BleachBreak struct is used to represent the behavior of a break statement during runtime. During the
+ * execution of a Bleach program, if a break statement is found, then such struct will be thrown as if it was a
+ * runtime error. Out of a loop structure, the Bleach Interpreter tries to catch this type of "runtime error"
+ * in order to signal that a loop has finished its execution due to a break statement.
+ */
 struct BleachBreak{};
