7a71e8dfa7d91c19ad1db154868e51b74e29a6f4
Victor Miguel <vmmc5910@gmail.com>
2024-08-19 10:06:11 -0300

    Add the complete documentation of the 'BleachContinue' struct from the 'BleachContinue.hpp' file.

--- src/utils/BleachContinue.hpp
+++ src/utils/BleachContinue.hpp
@@ -1,3 +1,13 @@
 #pragma once
 
+/**
+ * @struct BleachContinue
+ * @brief Represents a continue statement during runtime (the execution of a Bleach program).
+ *
+ * The BleachContinue struct is used to represent the behavior of a continue statement during runtime. During 
+ * the execution of a Bleach program, if a continue statement is found, then such struct will be thrown as if it 
+ * was a runtime error. Inside a loop structure, the Bleach Interpreter tries to catch this type of "runtime 
+ * error" in order to signal that a loop has stopped the execution of its body in the middle of it and needs
+ * to immediately go to its next iteration.
+ */
 struct BleachContinue{};
