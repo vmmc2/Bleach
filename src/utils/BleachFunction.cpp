8627c731cc182eb2ce0d70abb7dab98640d8b16d
Victor Miguel <vmmc5910@gmail.com>
2024-08-19 21:05:19 -0300

    Add complete documentation of the 'BleachFunction' class from the 'BleachFunction.hpp' and 'BleachFunction.cpp' files.

--- src/utils/BleachFunction.cpp
+++ src/utils/BleachFunction.cpp
@@ -6,14 +6,58 @@
 #include "./Stmt.hpp"
 
 
-BleachFunction::BleachFunction(std::shared_ptr<Function> functionDeclaration, std::shared_ptr<Environment> closure, bool isInitializer)
+/**
+ * @brief Constructs a BleachFunction object. 
+ *
+ * This constructor initializes a BleachFunction object with the three attributes that were mentioned inside the
+ * "BleachFunction.hpp" file.
+ *
+ * @param isInitializer: A boolean that signals whether or not the instance of the BleachFunction is a 
+ * constructor of a class, also known as the "init" method (represented by the bool type).
+ * @param closure: A pointer that points to an instance of the nearest enclosing instance of the Environment
+ * class (represented by the std::shared_ptr<Environment> type).
+ * @param functionDeclaration: A pointer that points to an instance of the Function class, which is the 
+ * corresponding static time representation of this instance of the BleachFunction class. In order words, the
+ * "functionDeclaration" attribute is a pointer to the AST node that represents its corresponding function declaration
+ * statement node (represented by the std::shared_ptr<Function> type).
+**/BleachFunction::BleachFunction(std::shared_ptr<Function> functionDeclaration, std::shared_ptr<Environment> closure, bool isInitializer)
   : functionDeclaration{std::move(functionDeclaration)}, closure{std::move(closure)}, isInitializer{isInitializer}
 {}
 
+/**
+ * @brief Returns the arity (amount of the arguments expected) when calling the BleachFunction object during 
+ * runtime.
+ * 
+ * This method is responsible for returning the arity (amount of arguments expected) when calling an instance
+ * of the BleachFunction class during runtime. To verify the arity of the user-defined function, it checks the 
+ * corresponding declaration of the BleachFunction object that was parsed and returns the amount of parameters 
+ * present inside such declaration.
+ * 
+ * @return An integer that denotes the arity (the amount of arguments expected) for this function (represented 
+ * by the int type).
+**/
 int BleachFunction::arity(){
   return functionDeclaration->parameters.size();
 }
 
+/**
+ * @brief Receives an instance of the BleachInstance class and returns a new instance of this same BleachFunction
+ * class. The only difference, is that this new instance of the BleachFunction has an updated environment. This
+ * environment contains a binding between the "self" name and the received instance of the BleachInstance class. 
+ * 
+ * First of all, it's important to mention that this method will only be used by instances of the BleachFunction
+ * class that are representing methods. This method is responsible for receiving an instance of the 
+ * BleachInstance class, creating a new Environment whose closure is the same of this instance of the 
+ * BleachFunction class, then, in this new environment, it creates a binding between the name "self" and the
+ * instance of the BleachInstance class that was received as an argument. Finally, it returns a new instance
+ * of a BleachFunction class whose environment is this new one that was created.
+ * 
+ * @param instance: A pointer to an instance of the BleachInstance class (represented by the 
+ * std::shared_ptr<BleachInstance> type).
+ * 
+ * @return A pointer to the newly created instance of the BleachFunction class (represented by the 
+ * std::shared_ptr<BleachFunction> type).
+**/
 std::shared_ptr<BleachFunction> BleachFunction::bind(std::shared_ptr<BleachInstance> instance){
   auto environment = std::make_shared<Environment>(closure);
   environment->define("self", instance); // Declaring the variable "self" inside this new environment and defining its value to be equal to the instance that the method is being accessed from.
@@ -21,6 +65,25 @@ std::shared_ptr<BleachFunction> BleachFunction::bind(std::shared_ptr<BleachInsta
   return std::make_shared<BleachFunction>(functionDeclaration, environment, isInitializer); // Just pass on the original value of "isInitializer" to the newly created "BleachFunction" object.
 }
 
+/**
+ * @brief Executes the instance of the BleachFunction class and returns whatever value the user-defined function
+ * returns when executed.
+ * 
+ * This method is responsible for executing an instance of the BleachFunction class during runtime. The 
+ * execution of a BleachFunction object essentialy means the execution of an user-defined function or method 
+ * during runtime. This method will return whatever value the user-defined function or method returns. If 
+ * there's no return statement inside the user-defined function or method declaration AST node stored in the 
+ * instance of this class, then such function or method implicitly returns a nullptr (nil value in Bleach).
+ * 
+ * @param interpreter: The reference to the instance of the Interpreter class that is running the Bleach file
+ * (represented by the Interpreter& type).
+ * @param arguments: The list of arguments that are expected to be received by the user-defined function or 
+ * method during runtime (represented by the std::vector<std::any> type).
+ 
+ * @return The corresponding value that the user-defined function or method is supposed to return. Since such
+ * structures can return a value of any type, the Bleach interpreter needs to deal with this. Thus, the return
+ * type of this method is std::any.
+**/
 std::any BleachFunction::call(Interpreter& interpreter, std::vector<std::any> arguments){
   auto environment = std::make_shared<Environment>(closure); // Create an environment (scope) for the function that is about to be executed. The function environment has as its parent environment the closure that involves it.
 
@@ -44,12 +107,24 @@ std::any BleachFunction::call(Interpreter& interpreter, std::vector<std::any> ar
   return nullptr; // This here is necessary for the case when a function does not have a "return" statement. By default, all user defined functions in Bleach return nil (C++ nullptr).
 }
 
+/**
+ * @note: This overloaded version of the 'call' method is restricted to be used when calling Bleach native 
+ * functions. It won't be called by an instance of a BleachFunction class.
+**/
 std::any BleachFunction::call(Interpreter& interpreter, Token paren, std::vector<std::any> arguments){
  std::cout << "No implementation of this method available for the 'BleachFunction' class." << std::endl;
  
   return {};
 }
 
+/**
+ * @brief Returns the string representation of an instance of the BleachFunction class.
+ * 
+ * This method is responsible for returning the string representation of an instance of the BleachFunction class.
+ * 
+ * @return A string that is the string representation of an instance of the BleachFunction class (represented by
+ * the std::string type).
+**/
 std::string BleachFunction::toString(){
   return "<function " + functionDeclaration->name.lexeme + ">";
 }
--- src/utils/BleachFunction.hpp
+++ src/utils/BleachFunction.hpp
@@ -7,15 +7,32 @@
 
 #include "./BleachCallable.hpp"
 
-class BleachInstance;
-class Environment;
-class Function;
 
+class BleachInstance; // Forward declaration necessary to implement the BleachFunction class.
+class Environment; // Forward declaration necessary to implement the BleachFunction class.
+class Function; // Forward declaration necessary to implement the BleachFunction class.
+
+/**
+ * @class BleachFunction
+ * 
+ * @brief This class is responsible for representing, at runtime, a user-defined function inside a Bleach 
+ * program.
+ *  
+ * The BleachFunction class is responsible for providing a runtime representation of every user-defined function 
+ * in a Bleach program. This class has 3 attributes: The first one is "isInitializer". It is a boolean that 
+ * signals whether or not the instance of this class is a constructor method (remember that, in this interpreter
+ * implementation, there is no distinction between functions and methods during runtime). The second one is
+ * "closure". It is a pointer that refers to the instance of the Environment class that is nearest one that 
+ * encloses the declaration of this function/method. The third one is "functionDeclaration". It is a pointer that
+ * refers to an instance of the Function class. This instance of the Function class is the one that represents
+ * this instance of the BleachFunction during static time.
+**/
 class BleachFunction : public BleachCallable{
   private:
     bool isInitializer;
     std::shared_ptr<Environment> closure;
     std::shared_ptr<Function> functionDeclaration;
+    
   public:
     BleachFunction(std::shared_ptr<Function> functionDeclaration, std::shared_ptr<Environment> closure, bool isInitializer);
     int arity() override;
